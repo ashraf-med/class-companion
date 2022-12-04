8b6feb234b4b996c585e27f235ff75ee1d2a8348
ashraf-med <118563557+ashraf-med@users.noreply.github.com>
2022-12-04 01:19:59 +0100

    latest update

---
+++ App.js
@@ -0,0 +1,48 @@
+// utilities
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Absences from "./pages/absences";
+import Error from "./pages/404";
+import Students from "./pages/students";
+import Teachers from "./pages/teachers";
+import Sesions from "./pages/sesions";
+import StudentsByGroup from "./pages/students-by-group";
+import MyAbsences from "./pages/my-absences";
+import Justifications from "./pages/justifications";
+
+
+function App() {
+
+  const loggedIn = window.localStorage.getItem('loggedIn');
+
+  return (
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={(loggedIn) ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/teachers" element={<Teachers />} />
+            <Route path="/absences" element={<Absences />} />
+            <Route path="/sesions" element={< Sesions/>} />
+            <Route path="/justifications" element={< Justifications/>} />
+            <Route path="/studentsbygroup" element={< StudentsByGroup/>} />
+            <Route path="/my-absences" element={< MyAbsences/>} />
+            <Route path="/*" element={<Error />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
