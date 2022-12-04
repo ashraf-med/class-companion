ae3c673638008258aaceb707d39f64581b67be31
ashraf-med <118563557+ashraf-med@users.noreply.github.com>
2022-12-04 01:23:25 +0100

    latest update

---
+++ pages/justifications.js
@@ -0,0 +1,146 @@
+//import style
+import "../styles/home.css";
+//import partials
+import Header from "../partials/header";
+import Loading from "../partials/loading";
+import SideMenu from "../partials/side-menu";
+import JustificationCard from "../partials/justification_card";
+
+//import utilities
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Justifications = () =>{
+
+  const navigate = useNavigate();
+
+  const loggedIn = window.localStorage.getItem("loggedIn");
+  const accountType = window.localStorage.getItem("accountType");
+
+  const [justificationList, setJustificationList] = useState([]);
+  const [studentsList, setStudentsList] = useState([]);
+  const [teachersList, setTeachersList] = useState([]);
+
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login");
+    } else {
+      setIsLoading(true);
+      Axios.get("http://localhost:3001/students")
+        .then((res) => {
+          console.log(res.data);
+          setStudentsList(res.data);
+       
+          Axios.get("http://localhost:3001/teachers")
+            .then((res) => {
+              console.log(res.data);
+              setTeachersList(res.data);
+              setTimeout(() => {
+                Axios.get("http://localhost:3001/justifications")
+                  .then((res) => {
+                    console.log(res.data);
+                    setJustificationList(res.data);
+                    setIsLoading(false);
+                    
+                  })
+                  .catch((err) => {
+                    setError(err);
+                    console.log(err);
+                    setIsLoading(false);
+                  });
+              }, 1000);
+            })
+            .catch((err) => {
+              setError(err);
+              console.log(err);
+              setIsLoading(false);
+            });
+        })
+        .catch((err) => {
+          setError(err);
+          console.log(err);
+          setIsLoading(false);
+        });
+        
+
+    }
+  }, []);
+
+  return(
+    <div className="home-page">
+      <Header />
+
+      <div className="home">
+        <SideMenu type={accountType} page="justifications" />
+
+        <div className="main-page">
+          <div className="settings">
+            <h1>justifications</h1>
+            <form className="filters">
+              <select
+                name="filter-by-speciality"
+                id="filter-by-speciality"
+                defaultValue=""
+              >
+                <option disabled value="">
+                  Filter by speciality
+                </option>
+                <option value="TI">TI</option>
+                <option value="GL">GL</option>
+                <option value="SCI">SCI</option>
+                <option value="SI">SI</option>
+              </select>
+              <select
+                name="filter-by-group"
+                id="filter-by-group"
+                defaultValue=""
+              >
+                <option disabled value="">
+                  Filter by group
+                </option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <div>
+                <input
+                  type="checkbox"
+                  name="order-alphabetically"
+                  id="order-alphabetically"
+                />
+                <label htmlFor="order-alphabetically">
+                  Order alphabetically
+                </label>
+              </div>
+            </form>
+          </div>
+          {error && <h1>{error}</h1>}
+          {isLoading && <Loading />}
+          {!isLoading && !error && (
+              <div className="cards">
+                {justificationList.map((justification) => (
+                  <JustificationCard
+                    key={justification._id}
+                    
+                    justification={justification}
+                    studentsList={studentsList}
+                    teachersList={teachersList}
+                  />
+                 
+                ))}
+              </div>
+            )}
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Justifications
